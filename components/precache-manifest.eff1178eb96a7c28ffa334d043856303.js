e0bd584fd550fac3480c9052efea1d0761c984ec
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-08-12 15:54:49 +0000

    Deploy teanocrata/mi2hoofs to github.com/teanocrata/mi2hoofs.git:gh-pages

---
+++ components/precache-manifest.eff1178eb96a7c28ffa334d043856303.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5bb587097147cb69c796142ac3fa7978",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.2d7daf97f13e5a68f0a9.bundle.js"
+  },
+  {
+    "url": "runtime~main.2d7daf97f13e5a68f0a9.bundle.js"
+  },
+  {
+    "url": "vendors~main.2d7daf97f13e5a68f0a9.bundle.js"
+  },
+  {
+    "url": "vendors~main.2d7daf97f13e5a68f0a9.bundle.js.LICENSE.txt"
+  }
+]);
---
+++ precache-manifest.39568906e2340bbb5c7c8e0589a8f1af.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7e6ff3748edcd5dc66d6cc4be4fbad0",
+    "url": "/bluehoofs/index.html"
+  },
+  {
+    "revision": "71aea99f7348b3c55337",
+    "url": "/bluehoofs/static/css/2.945abe7b.chunk.css"
+  },
+  {
+    "revision": "6f897dd7495c8cb455a1",
+    "url": "/bluehoofs/static/css/main.cb0bfdc6.chunk.css"
+  },
+  {
+    "revision": "71aea99f7348b3c55337",
+    "url": "/bluehoofs/static/js/2.35b764d1.chunk.js"
+  },
+  {
+    "revision": "e18637604789af6a40503615de5bd745",
+    "url": "/bluehoofs/static/js/2.35b764d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f897dd7495c8cb455a1",
+    "url": "/bluehoofs/static/js/main.a9597f23.chunk.js"
+  },
+  {
+    "revision": "8c4012c254bdfb5ab919",
+    "url": "/bluehoofs/static/js/runtime-main.8f4e6b5c.js"
+  },
+  {
+    "revision": "05f1cdadfe476395f60e233b15c22155",
+    "url": "/bluehoofs/static/media/icons-16.05f1cdad.eot"
+  },
+  {
+    "revision": "3c1c220e7a18286503fb431c7a7fe183",
+    "url": "/bluehoofs/static/media/icons-16.3c1c220e.woff"
+  },
+  {
+    "revision": "3cde8748332d1de6b1ae1c2dc5850754",
+    "url": "/bluehoofs/static/media/icons-16.3cde8748.ttf"
+  },
+  {
+    "revision": "0a5c76518a68c185baa2c6744456918c",
+    "url": "/bluehoofs/static/media/icons-20.0a5c7651.eot"
+  },
+  {
+    "revision": "51ec31f302d0072808e1f83f85fea4cd",
+    "url": "/bluehoofs/static/media/icons-20.51ec31f3.ttf"
+  },
+  {
+    "revision": "cef8cdbb9d0ba82e6e19fb0eeba2ac3d",
+    "url": "/bluehoofs/static/media/icons-20.cef8cdbb.woff"
+  }
+]);
